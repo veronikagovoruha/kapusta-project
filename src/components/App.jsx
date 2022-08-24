d278c8ca84b178fca3886bdc6d433d213d909960
Viktoriia Nistorika <vnistorika@gmail.com>
2022-08-24 18:24:53 +0300

    upd chart

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,11 @@
+import ChartBar from './chartBar/ChartBar';
+
+// const data = [{transport : 500}, {meat: 900}, {dog: 2500}]
+
 const App = () => {
-  return <div></div>;
+  return <div>
+    <ChartBar />
+  </div>;
 };
 
 export default App;
