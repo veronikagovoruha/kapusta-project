edba4ab4045b2fe4fcb62c55dacb71b48140fa2f
SergejShk <sshkurupij11@gmail.com>
2022-08-24 18:39:39 +0300

    merged branches

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,5 @@
-import ChartBar from './chartBar/ChartBar';
-
-// const data = [{transport : 500}, {meat: 900}, {dog: 2500}]
-
 const App = () => {
-  return <div>
-    <ChartBar />
-  </div>;
+  return <div></div>;
 };
 
 export default App;
