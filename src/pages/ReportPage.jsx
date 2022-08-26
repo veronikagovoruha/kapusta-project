5f0c06f4c745022f50ee6098bbea1b203c17ed3f
SergejShk <sshkurupij11@gmail.com>
2022-08-26 10:50:27 +0300

    connect expenses report

--- src/pages/ReportPage.jsx
+++ src/pages/ReportPage.jsx
@@ -1,4 +1,5 @@
 import ChartBar from 'components/chartBar/ChartBar';
+import Expenses from 'components/Expenses/Expenses';
 import Section from 'components/Section/Section';
 import { Link, useLocation } from 'react-router-dom';
 
@@ -8,6 +9,7 @@ const ReportPage = () => {
   return (
     <Section>
       <Link to={location.state ?? '/balance'}>GO BACK</Link>
+      <Expenses />
       <ChartBar />
     </Section>
   );
