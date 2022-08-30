a3d82df979c76e0d4b90d2bd58fd7ce76bf87db2
Veronika <veronikagovoruha@gmail.com>
2022-08-30 11:27:11 +0300

    change base path

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/kapusta-project/">
+        <BrowserRouter basename="/kapusta/">
           <App />
         </BrowserRouter>
       </PersistGate>
