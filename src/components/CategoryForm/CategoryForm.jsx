20a9eba3cef715dc5c36bbd3f18139ae49c6c7d5
Konstantin Popov <popov.k.g@gmail.com>
2022-08-30 08:59:02 +0300

    fixed according 3rd and 4th mentors comments

--- src/components/CategoryForm/CategoryForm.jsx
+++ src/components/CategoryForm/CategoryForm.jsx
@@ -10,6 +10,14 @@ import {
 } from '../../redux/transactions/transactionsOperations';
 import DatePicker from '../DatePickerForm/DatePicker';
 import { getDate } from '../../redux/dynamicData/dynamicDataSelector';
+import {
+  optionsExpenses,
+  optionsIncomes,
+} from '../../utils/options/transactionsCategoryOptions.js';
+import {
+  customStylesMobile,
+  customStylesTablet,
+} from '../../utils/options/selectStylesOptions';
 import sprite from '../../assets/icons/sprite.svg';
 import s from './CategoryForm.module.css';
 
@@ -23,95 +31,8 @@ const CategoryForm = () => {
 
   const location = useLocation();
 
-  const optionsExpenses = [
-    { value: 'Транспорт', label: 'Transport' },
-    { value: 'Продукты', label: 'Products' },
-    { value: 'Здоровье', label: 'Health' },
-    { value: 'Алкоголь', label: 'Alcohol' },
-    { value: 'Развлечения', label: 'Entertainment' },
-    { value: 'Всё для дома', label: 'Housing' },
-    { value: 'Техника', label: 'Technique' },
-    { value: 'Коммуналка и связь', label: 'Communal, communication' },
-    { value: 'Спорт и хобби', label: 'Sports, hobbies' },
-    { value: 'Образование', label: 'Education' },
-    { value: 'Прочее', label: 'Other' },
-  ];
-
-  const optionsIncomes = [
-    { value: 'З/П', label: 'Salary' },
-    { value: 'Доп. доход', label: 'Extra earn' },
-  ];
-
   const isMobile = useMediaQuery({ maxWidth: 767 });
   const isDesktopOrTablet = useMediaQuery({ minWidth: 768 });
-
-  const customStylesMobile = {
-    control: base => ({
-      ...base,
-      width: 240,
-      minHeight: 10,
-      paddingLeft: 0,
-      textAlign: 'left',
-      border: 'none',
-      backgroundColor: '#F5F6FB',
-    }),
-    indicatorSeparator: base => ({
-      ...base,
-      width: 0,
-    }),
-    placeholder: base => ({
-      ...base,
-      paddingLeft: 0,
-      color: '#C7CCDC',
-    }),
-    container: base => ({
-      ...base,
-      paddingLeft: 18,
-    }),
-    valueContainer: base => ({
-      ...base,
-      paddingLeft: 0,
-    }),
-    input: base => ({
-      ...base,
-      marginLeft: 0,
-      paddingLeft: 2,
-    }),
-  };
-
-  const customStylesTablet = {
-    control: base => ({
-      ...base,
-      width: 150,
-      minHeight: 10,
-      paddingLeft: 0,
-      textAlign: 'left',
-      border: 'none',
-      backgroundColor: '#FFF',
-    }),
-    indicatorSeparator: base => ({
-      ...base,
-      width: 0,
-    }),
-    placeholder: base => ({
-      ...base,
-      paddingLeft: 0,
-      color: '#C7CCDC',
-    }),
-    container: base => ({
-      ...base,
-      paddingLeft: 18,
-    }),
-    valueContainer: base => ({
-      ...base,
-      paddingLeft: 0,
-    }),
-    input: base => ({
-      ...base,
-      marginLeft: 0,
-      paddingLeft: 2,
-    }),
-  };
 
   const transactionData = {
     description,
